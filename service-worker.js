15c46c73059a541a82b3a9304c0e71f67c7f1590
Ignacio Menárguez Fernández <info@imenarguez-ia.com>
2026-05-11 17:11:51 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,23 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('v1').then(c=>c.addAll(['/','/index.html','/manifest.json'])))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE_NAME = 'menarguez-ia-v2';
+const urlsToCache = ['/'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() =>
+    caches.match(event.request)
+  ));
+});
